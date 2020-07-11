9894f933ed310728b4d8f478bd1157d1bfd4bb30
divyanshk443 <55971596+divyanshk443@users.noreply.github.com>
2020-07-12 03:40:18 +0530

    Add files via upload

---
+++ tests/go_stmt.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+        "fmt"
+)
+func f() int {
+        
+
+        return 567
+}
+
+func main() {
+
+        go f() 
+}
+
+~                                                                                                                                              
+~                                                                                                                                              
+~         

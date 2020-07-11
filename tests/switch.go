a5023bad2cfa3ac94d1398a3b902b8a73254eac9
driti924 <55576453+driti924@users.noreply.github.com>
2020-07-12 02:16:59 +0530

    Create switch.go

---
+++ tests/switch.go
@@ -0,0 +1,26 @@
+package main 
+  
+import "fmt"
+  
+func main() { 
+      var day int = 2  
+    switch day{ 
+       case 1: 
+       fmt.Println("Monday") 
+       case 2: 
+       fmt.Println("Tuesday") 
+       case 3: 
+       fmt.Println("Wednesday") 
+       case 4: 
+       fmt.Println("Thursday") 
+       case 5: 
+       fmt.Println("Friday") 
+       case 6: 
+       fmt.Println("Saturday") 
+       case 7: 
+       fmt.Println("Sunday") 
+       default:  
+       fmt.Println("Invalid") 
+   } 
+     
+} 

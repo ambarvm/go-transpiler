c4e050d5d8a521d5f8f0e2a007d854d7cb93a0ee
divyanshk443 <55971596+divyanshk443@users.noreply.github.com>
2020-07-12 02:04:29 +0530

    Add files via upload

---
+++ tests/vardecl_shorthand.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {  
+    count := 10
+    fmt.Println("Count =",count)
+}
---
+++ tests/vardecl_var.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {  
+    var age int // variable declaration
+    fmt.Println("My age is", age)
+}
